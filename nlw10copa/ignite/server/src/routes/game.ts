03449078c33b3855421d521e2119e14e63a726da
Gustavo Bretas <gustavobretas@gmail.com>
2022-11-26 18:46:59 -0400

    Add Endpoint Related to Guesses

--- nlw10copa/ignite/server/src/routes/game.ts
+++ nlw10copa/ignite/server/src/routes/game.ts
@@ -9,7 +9,7 @@ export async function gameRoutes(fastify : FastifyInstance) {
         return { count };
     });
 
-    fastify.get('/pools/:id/games', {onRequest: [authenticate]}, async (request) => {
+    fastify.get('/pools/:id/games', async (request) => {
         const getPollParams = z.object({
             id: z.string(),
         });
@@ -40,4 +40,72 @@ export async function gameRoutes(fastify : FastifyInstance) {
             }
         }) };
     });
+
+    fastify.post('/pools/:poolsId/games/:gameId/guesses', { onRequest: [authenticate] }, 
+        async (request, reply) => {
+            const createGuessParamsSchema = z.object({
+                poolsId: z.string(),
+                gameId: z.string(),
+            });
+
+            const createGuessBodySchema = z.object({
+                firstTeamScore : z.number(),
+                secondTeamScore : z.number(),
+            });
+
+            const { poolsId, gameId } = createGuessParamsSchema.parse(request.params);
+            const { firstTeamScore, secondTeamScore } = createGuessBodySchema.parse(request.body);
+
+            const participant = await prisma.participant.findUnique({
+                where: {
+                    userId_poolId: {
+                        userId: request.user.sub,
+                        poolId: poolsId,
+                    }
+                }
+            });
+
+            if (!participant) {
+                return reply.status(404).send({ message: 'Participant not found at this Poll.' });
+            }
+
+            const guess = await prisma.guess.findUnique({
+                where: {
+                    gameId_participantId: {
+                        participantId: participant.id,
+                        gameId,
+                    }
+                }
+            });
+
+            if (guess) {
+                return reply.status(400).send({ message: 'You have already guessed this game at this Poll.' });
+            };
+
+            const game = await prisma.game.findUnique({
+                where: {
+                    id: gameId,
+                }
+            });
+
+            if (!game) {
+                return reply.status(404).send({ message: 'Game not found.' });
+            };
+
+            if (game.date < new Date()) {
+                return reply.status(400).send({ message: 'You can not guess a game that has already started.' });
+            };
+
+            await prisma.guess.create({
+                data: {
+                    gameId,
+                    participantId: participant.id,
+                    firstTeamScore,
+                    secondTeamScore,
+                }
+            });
+
+            return reply.status(201).send({ message: 'Guess created successfully.' });
+        }
+    );
 };
