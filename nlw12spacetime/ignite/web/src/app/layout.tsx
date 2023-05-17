2f9b58af60e39790d7f8802fc1b9065479d5e803
Gustavo Bretas <gustavobretas@gmail.com>
2023-05-17 15:58:30 -0400

    nlw12 Website Layout Partial

--- nlw12spacetime/ignite/web/src/app/layout.tsx
+++ nlw12spacetime/ignite/web/src/app/layout.tsx
@@ -1,18 +1,31 @@
 import { ReactNode } from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
 
 export const metadata = {
   title: 'NLW 11 Spacetime / Ignite',
-  description: 'Generated Rocketseat Ignite',
+  description:
+    'Uma capsula do tempo construida com ReactJS, NextJS, TailwindCSS e Tyhpescript',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${roboto.className} ${baiJamjuree.className} bg-gray-900 font-sams text-gray-100`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
