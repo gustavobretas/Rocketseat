686fa3071a5df7598fedcc3034dcbdad1a83168e
Gustavo Bretas <gustavobretas@gmail.com>
2023-05-20 14:27:48 -0400

    nlw12 Website Login and Create Account

--- nlw12spacetime/ignite/web/src/app/page.tsx
+++ nlw12spacetime/ignite/web/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from 'next/headers'
+
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemory } from '@/components/EmptyMemory'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticaded = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -12,7 +17,7 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
         {/* Stripes */}
         <div className=" absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
-        <SignIn />
+        {isAuthenticaded ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
---
+++ nlw12spacetime/ignite/web/src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import { getUser } from '@/lib/Auth'
+import Image from 'next/image'
+
+export function Profile() {
+  const { name, avatarUrl } = getUser()
+
+  return (
+    // SignIn
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt="User Avatar"
+        className="h-10 w-10 rounded-full"
+      />
+      <p className="max-w-[150px] text-sm leading-snug">
+        {name}
+        <a href="" className="block text-red-400 hover:text-red-300">
+          Quero sair
+        </a>
+      </p>
+    </div>
+  )
+}
