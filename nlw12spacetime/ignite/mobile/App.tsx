f88e2aa72079a129c92e55c51cb3f99685527e27
Gustavo Bretas <gustavobretas@gmail.com>
2023-05-23 06:41:31 -0400

    nlw12 Mobile JTW Partial

--- nlw12spacetime/ignite/mobile/App.tsx
+++ nlw12spacetime/ignite/mobile/App.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 
 import {
   useFonts,
@@ -16,12 +18,38 @@ import { styled } from 'nativewind'
 
 const StyledStripes = styled(Stripes)
 
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/41260f6271cdaecd1483',
+}
+
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signInWithGitHub] = useAuthRequest(
+    {
+      clientId: '41260f6271cdaecd1483',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      console.log(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -48,6 +76,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGitHub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar Lembrança
