c3b74ecd3553762ddc6b11633052a7e1a81af059
Gustavo Bretas <gustavobretas@gmail.com>
2023-09-05 09:32:51 -0400

    Setup do Projeto FinAPI, e criação da Roda de Cadastro de Cliente

---
+++ Ignite/Nodejs-Trilha-2022/finapi-express/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+
+customers = [];
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+    
+    return response.status(201).send({ message: "Account created!" });
+});
+
+app.listen(3333);
