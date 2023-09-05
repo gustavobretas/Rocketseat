2ce74fbc8b09d66e84a55bf0ba5f7f53465f23ab
Gustavo Bretas <gustavobretas@gmail.com>
2023-09-04 23:22:53 -0400

    Adicionado Nodemon no Projeto

--- Ignite/Nodejs-Trilha-2022/fundamentos-nodejs/src/index.js
+++ Ignite/Nodejs-Trilha-2022/fundamentos-nodejs/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World, Ignite!' });
+    return response.json({ message: 'Hello World, Ignite! Fundamentos de NodeJS' });
 });
 
 app.listen(3333);
