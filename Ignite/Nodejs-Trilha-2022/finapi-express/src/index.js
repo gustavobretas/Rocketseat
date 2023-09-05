0aac55822e5028299a19b52137fa3da5c3a6f890
Gustavo Bretas <gustavobretas@gmail.com>
2023-09-05 10:15:16 -0400

    Ignite FinApi Criação de Rota para Saque, considerando o Saldo da Conta do Cliente

--- Ignite/Nodejs-Trilha-2022/finapi-express/src/index.js
+++ Ignite/Nodejs-Trilha-2022/finapi-express/src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === "credit") {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
 
@@ -63,6 +75,28 @@ app.post('/deposit', (request, response) => {
     customer.statement.push(statementOperation);
 
     return response.status(201).send({ message: "Deposit created!" });
+});
+
+app.post('/withdraw', (request, response) => {
+    const { amount } = request.body;
+
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" });
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send({ message: "Withdraw created!" });
 });
 
 app.listen(3333);
